fdead038152ea3cfbb25d6e89046a073160884af
Jin Cheng <jincheng88@outlook.com>
2017-04-25 14:31:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function getFirstSelector(selector){
+   return document.querySelector(selector);
+}
+function nestedTarget(){
+  return document.querySelector(".target");
+}
+function deepestChild(){
+  const divs = document.getElementById("grand-node").querySelectorAll("div");
+  return divs[divs.length-1];
+}
+function increaseRankBy(n){
+  const lis = document.querySelectorAll('.ranked-list');
+  var liss = [];
+  for (let i = 0, l = lis.length; i < l; i++) {
+  liss[i] = lis[i].parseInt+n;
+  lis[i].innerHTML = liss[i].toString()
+  }
+}
